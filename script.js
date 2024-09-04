186ad66c27d1e6a579a42afd5417572929a10c93
nramirez713 <noemiscorpio1998@yahoo.com>
2024-09-04 18:57:55 -0500

    linking

--- script.js
+++ script.js
@@ -32,19 +32,19 @@ function get_answer(category) {
 // Function to find the spans that need to be filled
 function fill_in_answers(answers) {
 	var home = document.querySelector('#home');  // This says make a new variable and find the HTML tag that has the ID of "home" 
-	var answer_2 = document.querySelector('#answer_2');
-	var answer_3 = document.querySelector('#answer_3');
-	var answer_1 = document.querySelector('#answer_1');
+	var career = document.querySelector('#career');
+	var animal = document.querySelector('#animal');
+	var place = document.querySelector('#place');
 	
 	// Fill them with the provided answers
 	home.innerText = answers['mash'];
-	answer_2.innerText = answers['answer_2'];
-	answer_3.innerText = answers['answer_3'];
-	answer_1.innerText = answers['answer_1'];
+	career.innerText = answers['career'];
+	animal.innerText = answers['animal'];
+	place.innerText = answers['place'];
 	home.innerHTML = answers.mash;  // Change the content of the element in the HTML doc with the id "home" to the "mash" value in answers 
-	answer_2.innerHTML = answers.answer_2;  // Change the content of the element in the HTML doc with the id "career" to the "career" value in answers 
-	answer_3.innerHTML = answers.answer_3;
-	answer_1.innerHTML = answers.answer_1;
+	career.innerHTML = answers.career;  // Change the content of the element in the HTML doc with the id "career" to the "career" value in answers 
+	animal.innerHTML = answers.animal;
+	place.innerHTML = answers.place;
 }
 
 // Button submit handler function
@@ -55,9 +55,9 @@ function handle_submission(evt) {
 	// Build up our answers object
 	var answers = {
 	    'mash': mash_choice(),
-	    'answer_2': get_answer('answer_2'),
-	    'answer_3': get_answer('answer_3'),
-	    'answer_1': get_answer('answer_1')
+	    'career': get_answer('career'),
+	    'animal': get_answer('animal'),
+	    'place': get_answer('place')
 	}
 	// Fill in the answers
 	fill_in_answers(answers);
